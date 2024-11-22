e0ab5fe13cb04a61ed1bb9065feca66324cffa64
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-11-22 08:22:15 +0100

    <feat>Added code to fetch data from pokemonAPI and importing the result array of pokemonData to App.jsx

---
+++ src/components/data.jsx
@@ -0,0 +1,61 @@
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+
+async function fetchPokemon(url) {
+    let array = [];
+    try {
+      const response = await fetch(url, {mode: 'cors'});
+      if(!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+      const data = await response.json();
+      data.results.forEach( async (pokemon) => {
+        let data = await fetchPokemonData2(pokemon);  //with async await it returns an object
+        array.push(data)
+    });
+    } catch(error) {
+        alert(error)
+    }
+    return array;  
+}
+
+const data = await fetchPokemon(url);
+
+async function fetchPokemonData2 (pokemon) {
+    let pokemonUrl = pokemon.url;
+    try {
+        const response = await fetch(pokemonUrl);
+        if(!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+          }
+        const data = await response.json();
+        const pokeData = {id: crypto.randomUUID(), name: data.name, img: data.sprites.front_shiny};
+        // console.log(pokeData); //Returns an object
+        return pokeData;
+      } catch(error) {
+          alert(error)
+      }
+}
+
+function fetchPokemonCards(url) {
+    let pokemonArray = [];
+    fetch(url)
+    .then(response => response.json())
+    .then(function(allpokemon){
+      allpokemon.results.forEach(function(pokemon){
+        fetchPokemonData(pokemonArray, pokemon); 
+      })
+     })
+     return pokemonArray;
+  }
+  
+  function fetchPokemonData(array, pokemon){
+    let url = pokemon.url // <--- this is saving the pokemon url to a variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+      fetch(url)
+      .then(response => response.json())
+      .then(function(pokeData){
+      array.push({id: crypto.randomUUID(), name: pokeData.name, img: pokeData.sprites.front_shiny})
+      })
+    }
+
+const cards = fetchPokemonCards(url);  
+export {cards, data};
